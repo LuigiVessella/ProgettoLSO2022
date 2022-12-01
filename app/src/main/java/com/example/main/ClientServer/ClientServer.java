6b8913749b0ce27dd6f86cf814be28093fc12926
Luigi Vessella <vessellaluigi12@gmail.com>
2022-12-01 16:29:38 +0100

    prima prova socket

--- app/src/main/java/com/example/main/ClientServer/ClientServer.java
+++ app/src/main/java/com/example/main/ClientServer/ClientServer.java
@@ -1,30 +1,90 @@
 package com.example.main.ClientServer;
 
-import java.io.DataInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
 
-public class ClientServer {
+public class ClientServer
+{
+    private String host = null;
+    private int port = 0;
+    private Socket socket = null;
+    private BufferedReader inStream = null;
+    private OutputStream ostream = null;
+    private PrintWriter pwrite = null;
 
-    public void foo(){
-        String str;
-        try {
+    public ClientServer(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+    public void setUp()
+    {
+        System.out.println("TalkToC.setUp() invoked");
 
-            ServerSocket ss = new ServerSocket(6666);
-            System.out.println("Socket in attesa di messaggi");
-            Socket s = ss.accept();
-            do {
-                DataInputStream dis = new DataInputStream(s.getInputStream());
-                str = (String)dis.readUTF();
-                System.out.println("message from client: " + str);
-
-            }while(str.equals("termina"));
-
-            ss.close();
+        try
+        {
+            socket = new Socket(host, port);
+            inStream = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
         }
-
-        catch(Exception e) {
+        catch(UnknownHostException e)
+        {
+            System.err.println("Cannot find host called: " + host);
             e.printStackTrace();
+            System.exit(-1);
+        }
+        catch(IOException e)
+        {
+            System.err.println("Could not establish connection for " + host);
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    public void sendSomeMessage(String stringa) {
+        try {
+            ostream = socket.getOutputStream();
+            pwrite = new PrintWriter(ostream, true);
+            pwrite.println(stringa);
+            pwrite.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void converse()
+    {
+        System.out.println("TalkToC.converse() invoked");
+
+        if (socket != null && inStream != null)
+        {
+            try
+            {
+                System.out.println(inStream.readLine());
+            }
+            catch(IOException e)
+            {
+                System.err.println("Conversation error with host " + host);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void cleanUp()
+    {
+        try
+        {
+            if (inStream != null)
+                inStream.close();
+            if (socket != null)
+                socket.close();
+        }
+        catch(IOException e)
+        {
+            System.err.println("Error in cleanup");
+            e.printStackTrace();
+            System.exit(-1);
         }
     }
 }
--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.main.ClientServer.ClientServer;
 
 public class MainActivity extends AppCompatActivity {
-    ClientServer clientServer = new ClientServer();
+    ClientServer client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +23,41 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (NullPointerException e){}
         setContentView(R.layout.activity_main);
+        client = new ClientServer("4.236.136.210", 8080);
+        initializeComponents();
     }
 
 
     public void initializeComponents(){
         Button btnSignin = findViewById(R.id.loginButton);
+        EditText someText = findViewById(R.id.emailLoginEditText);
 
         //Action after sign in
         btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String stringa = String.valueOf(someText.getText());
+                sendMessageToServer(stringa);
             }
         });
+
+    }
+
+    public void sendMessageToServer(String stringa){
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("sono qui");
+                client.setUp();
+                client.sendSomeMessage(stringa);
+                client.cleanUp();
+            }
+        });
+
+        thread.start();
+
+
 
     }
 }
