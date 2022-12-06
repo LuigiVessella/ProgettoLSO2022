b2ee09038d5663e6220ad1ff3a9feb5251a052ca
Luigi Vessella <vessellaluigi12@gmail.com>
2022-12-06 10:59:51 +0100

    geolocalizzazione funziona

--- app/src/main/java/com/example/main/PotholesActivity.java
+++ app/src/main/java/com/example/main/PotholesActivity.java
@@ -10,18 +10,22 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.main.Sensor.Accelerometer;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 public class PotholesActivity extends AppCompatActivity {
 
     private TextView welcomeTv;
+    private Button buttonGetPos;
+    private TextView latTv;
+    private TextView longTv;
 
     //la documentazione di android consiglia di usare questo, non sembra funzionare al momento
     private FusedLocationProviderClient fusedLocationClient;
@@ -34,31 +38,49 @@ public class PotholesActivity extends AppCompatActivity {
         } catch (NullPointerException e) {
         }
         setContentView(R.layout.activity_potholes);
+        requestPermission();
 
-        Toast.makeText(this, "SONO QUI", Toast.LENGTH_LONG).show();
+
         welcomeTv = findViewById(R.id.welcomeTextView);
+        buttonGetPos = findViewById(R.id.buttonGetPos);
+        latTv = findViewById(R.id.textViewLat);
+        longTv = findViewById(R.id.textViewLong);
 
-        Log.v("verifica", getIntent().getStringExtra("user"));
-        welcomeTv.setText("BENVENUTO " + getIntent().getStringExtra("user"));
+        welcomeTv.setText("Benvenuto, " + getIntent().getStringExtra("user"));
+
+        buttonGetPos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLastLoc();
+            }
+        });
         //Accelerometer acc = new Accelerometer(getApplicationContext(), PotholesActivity.this);
-
-        getLastLoc();
-
     }
 
     //questa è la funzione che dovrebbe prendere la posizione
-    @SuppressLint("MissingPermission")
+
     private void getLastLoc() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //se non si hanno i permessi, li richiediamo.
+            requestPermission();
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(PotholesActivity.this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 if (location != null) {
-
+                    latTv.setText( "lat: " + String.valueOf(location.getLatitude()));
+                    longTv.setText("long: " + String.valueOf(location.getLongitude()));
                 }
             }
         });
 
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
     }
 
 }
