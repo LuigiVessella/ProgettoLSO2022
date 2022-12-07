871d493b522ea88be70c1b303519bf975301bdc3
Luigi Vessella <vessellaluigi12@gmail.com>
2022-12-07 14:54:09 +0100

    sistemato current e last pos

--- app/src/main/java/com/example/main/PotholesActivity.java
+++ app/src/main/java/com/example/main/PotholesActivity.java
@@ -11,7 +11,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.main.Sensor.Accelerometer;
 import com.example.main.Sensor.Gps;
@@ -28,9 +31,9 @@ public class PotholesActivity extends AppCompatActivity {
     private TextView longTv;
     private TextView bucaTv;
     private Gps gps;
-
+    private LinearLayout registrazioniSv;
     private FusedLocationProviderClient fusedLocationClient;
-
+    private Integer counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,13 +49,13 @@ public class PotholesActivity extends AppCompatActivity {
     private void initializeComponents() {
         welcomeTv = findViewById(R.id.welcomeTextView);
         buttonGetPos = findViewById(R.id.buttonGetPos);
-        latTv = findViewById(R.id.textViewLat);
-        longTv = findViewById(R.id.textViewLong);
+        registrazioniSv = findViewById(R.id.linearScroll);
+
         bucaTv = findViewById(R.id.textViewBuca);
         Gps gps = new Gps(getApplicationContext(), PotholesActivity.this);
 
         welcomeTv.setText("Benvenuto, " + getIntent().getStringExtra("user"));
-
+        counter = 0;
         buttonGetPos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,7 +67,7 @@ public class PotholesActivity extends AppCompatActivity {
     }
 
     public void setCoordinate() {
-
+        counter++;
         bucaTv.setText("buca rilevata presso:");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -76,8 +79,13 @@ public class PotholesActivity extends AppCompatActivity {
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
-                            latTv.setText(String.valueOf(location.getLatitude()));
-                            longTv.setText(String.valueOf(location.getLongitude()));
+                            latTv = new TextView(PotholesActivity.this);
+                            longTv = new TextView(PotholesActivity.this);
+                            latTv.setText("trovata " + counter + " buca\n"+ "lat:" + String.valueOf(location.getLatitude()));
+                            longTv.setText("long: " + String.valueOf(location.getLongitude()) + "\n");
+                            registrazioniSv.addView(latTv);
+                            registrazioniSv.addView(longTv);
+
                             return;
                         }
                         else getCurrentPosIfNotLast();
@@ -98,11 +106,21 @@ public class PotholesActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
-                            latTv.setText(String.valueOf(location.getLatitude()));
-                            longTv.setText(String.valueOf(location.getLongitude()));
+                            Toast.makeText(PotholesActivity.this, "CURRENT POS", Toast.LENGTH_SHORT).show();
+                            latTv = new TextView(PotholesActivity.this);
+                            longTv = new TextView(PotholesActivity.this);
+                            latTv.setText("trovata " + counter + " buca\n"+ "lat:" + String.valueOf(location.getLatitude()));
+                            longTv.setText("long: " + String.valueOf(location.getLongitude()));
+                            registrazioniSv.addView(latTv);
+                            registrazioniSv.addView(longTv);
                             return;
                         }
                     }
                 });
+    }
+
+    private void sendQueryToServer(){
+        //qui il codice per mandare la query al server
+
     }
 }
